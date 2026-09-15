c45a067a34a9fd0ccfef427abfadcfb0363b820b
Noor Diab <nd00034@mix.wvu.edu>
2026-09-15 15:05:24 -0400

    Added the structure of the transition table and accepting table.

--- phase1/statetable.java
+++ phase1/statetable.java
@@ -1,8 +1,28 @@
-package phase1;
+package statetable.java;
 
 public class statetable {
+
+    
     public static void main (String[] args) {
         System.out.println("");
+
+        
     }
+
+    void get_transition_table(void)
+    {
+        int [][] transition_table = 
+        {0,1,-1	2,3	-1,-1,6,7,8,9,10,12,14,16,18,19,20,21,22};
+    }
+
+    void get_accepting_table(void)
+    {
+
+    }
+
+    
+    
+
+
 }
 
