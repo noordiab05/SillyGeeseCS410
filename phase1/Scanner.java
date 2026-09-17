7b229cda296eb5bf121e64473d6d324d05d08549
Noor Diab <nd00034@mix.wvu.edu>
2026-09-17 15:10:04 -0400

    Figured out file reading

--- phase1/Scanner.java
+++ phase1/Scanner.java
@@ -1,9 +1,25 @@
 package phase1;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Scanner {
-    
+    public static void main(String[] args) {
+
+        Path filePath = Path.of("");
+
+        try {
+            String fileContent = Files.readString(filePath);
+
+            for(int i=0; i<fileContent.length(); i++)
+            {
+                
+            }
+            
+            
+        } catch (Exception e) {
+        }
+
+        
+    }
 }
