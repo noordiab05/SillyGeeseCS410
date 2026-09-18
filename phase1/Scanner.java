01f1744b03ee7835763775566aca5f59676fd1fc
Noor Diab <nd00034@mix.wvu.edu>
2026-09-18 14:22:14 -0400

    Created an output.txt file, outputted the string, modified the transitionTable, and the getColumn() function.

--- phase1/Scanner.java
+++ phase1/Scanner.java
@@ -1,29 +1,83 @@
 package phase1;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Function;
 
 public class Scanner {
     public static void main(String[] args) {
 
-        Path filePath = Path.of("");
+        Path filePath = Path.of("DummyFile.txt");
 
         try {
             String fileContent = Files.readString(filePath);
 
+            StateTable tables = new StateTable();
+            int [][] transitionTable = tables.getTransitionTable();
+            boolean [] acceptingTable = tables.getAcceptingTable();
+            List<Function<String, String>> stateActionTable = tables.getStateActionTable();
 
             int state = 0; 
+            String input = "";
             for(int i=0; i<fileContent.length(); i++)
             {
 
                 char c= fileContent.charAt(i);
                 int columnIndex = getColumn(state, c);
+                
+                
+                //a transition aka a space is indicated by column index 0!!
+                if(columnIndex != 0)
+                {
 
+                    if(transitionTable[state][columnIndex] != -1)
+                    {
+                        state = transitionTable[state][columnIndex];
+                        input += c;
+                    }
+                    else if(acceptingTable[state] == true)
+                    {
+                        //output token
+                        try{
+                            Files.writeString(Path.of("Output.txt"), input);
+                            System.out.println("Successfully written.");
+
+                            input += c;
+
+                            
+                        }
+                        catch(IOException e)
+                        {
+                            System.out.println("Couldn't write to file.");
+                        }
+                       
+                        
+                        
+                    }
+                    else
+                    {
+                        //reject/crash
+                    }
+
+                }
+                else
+                {
+                    input = "";
+                }
+               
+
+
+                
+
+                
                 
             }
             
             
         } catch (Exception e) {
+            
         }
 
         
@@ -35,95 +89,79 @@ public class Scanner {
         StateTable table = new StateTable();
         int [][] transitionTable = table.getTransitionTable();
 
-        //checks a-zA-Z_, a-zA-z0-9_, 0, 1-9, 0-9
-        if((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>=0 && c<=9) || c=='_')
+        //checks a-zA-Z_, a-zA-z0-9
+        if((c>='a' && c<='z') || (c>='A' && c<='Z'))
         {
-            index = 2;
-
-            //check if there is a state here
-            if(transitionTable[row][index] == -1)
-            {
-                //check if alphabet
-                if((c>='a' && c<='z') || (c>='A' && c<='Z') || c=='_' )
-                {
-                    index = 1;
-                }
-
-                //check if numeric: 0-9
-                else if(c>=0 && c<=9)
-                {
-                    index = 5;
-
-                    //extra check for numeric: can be 0 or 1-9 (or simply 0-9)
-                    if(transitionTable[row][index] == -1)
-                    {
-                        if(c == 0)
-                        {
-                            index = 3;
-                        }
-                        else
-                        {
-                            index = 4;
-                        }
-                    }
-                }
-            }
+            index = 1;
+        }
+        else if(c=='0')
+        {
+            index =2;
+        }
+        else if (c>='1' && c<='9') 
+        {
+            index = 3;
         }
         else if(c=='.')
         {
-            index = 6;
+            index = 4;
         }
         else if(c == '+')
         {
-            index = 7;
+            index = 5;
         }
         else if(c=='-')
         {
-            index = 8;
+            index = 6;
         }
         else if(c == '*')
         {
-            index = 9;
+            index = 7;
         }
         else if(c == '/')
         {
-            index = 10;
+            index = 8;
         }
         else if(c=='=')
         {
-            index = 11;
+            index = 9;
         }
         else if(c=='<')
         {
-            index = 12;
+            index = 10;
         }
         else if(c == '>')
         {
-            index = 13;
+            index = 11;
         }
         else if(c =='!')
         {
-            index = 14;
+            index = 12;
         }
         else if(c=='{')
         {
-            index = 15;
+            index = 13;
         }
         else if(c == '}')
         {
-            index = 16;
+            index = 14;
         }
         else if(c == '(')
         {
-            index = 17;
+            index = 15;
         }
         else if(c == ')')
         {
-            index = 18;
+            index = 16;
         }
         else if(c == ';')
         {
-            index = 19;
+            index = 17;
+        }
+        else if(c == ' ')
+        {
+            //this means a transition is occuring
+            index = 0;
         }
 
         
